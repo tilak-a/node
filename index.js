bf163da47dbea7f8bb0c202ef73aa8f1d27b9d0f
vijayj <119652401+vijayjdotpitchtechnologies@users.noreply.github.com>
2024-03-28 19:15:32 +0530

    videos

--- index.js
+++ index.js
@@ -157,97 +157,89 @@ const userSocketMap = {};
 io.on("connection", socket => {
   console.log("Socket connected:", socket.id);
 
-  socket.on("joinRoom", roomId => {
-    socket.join(roomId);
-    socket.roomId = roomId;
-    socket.broadcast.to(roomId).emit("userJoined", socket.id); // Emit userJoined event
-  });
-
-  socket.on("offer", offer => {
-    // Broadcast the offer to all connected clients except the sender
-    socket.to(offer.target).emit("offer", offer);
-  });
-
-  socket.on("answer", answer => {
-    // Broadcast the answer to all connected clients except the sender
-    socket.to(answer.target).emit("answer", answer);
-  });
-
-  socket.on("iceCandidate", candidate => {
-    // Broadcast the ICE candidate to all connected clients except the sender
-    socket.to(candidate.target).emit("iceCandidate", candidate);
-  });
-
-  // Handle disconnection
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-    socket.broadcast.to(socket.roomId).emit("userLeft", socket.id);
-  });
-
   // Handle when a user sends a message
-  // socket.on("msg", async ({ msg, recipientId, senderId, name }) => {
-  //   try {
-  //     // Ensure that the senderId and recipientId are provided
-  //     if (!senderId || !recipientId) {
-  //       throw new Error("Sender ID or recipient ID is missing");
-  //     }
+  socket.on("msg", async ({ msg, recipientId, senderId, name }) => {
+    try {
+      // Ensure that the senderId and recipientId are provided
+      if (!senderId || !recipientId) {
+        throw new Error("Sender ID or recipient ID is missing");
+      }
 
-  //     // Emit the message to the recipient
-  //     const recipientSocketId = userSocketMap[recipientId];
-  //     if (recipientSocketId) {
-  //       io.to(recipientSocketId).emit("recive", {
-  //         message: msg,
-  //         senderId,
-  //         recipientId,
-  //         name,
-  //         timestamp: Date.now()
-  //       });
-  //       console.log("map", userSocketMap);
-  //     } else {
-  //       console.log("Recipient socket not found for user ID:", recipientId);
-  //     }
+      // Emit the message to the recipient
+      const recipientSocketId = userSocketMap[recipientId];
+      if (recipientSocketId) {
+        io.to(recipientSocketId).emit("recive", {
+          message: msg,
+          senderId,
+          recipientId,
+          name,
+          timestamp: Date.now()
+        });
+        console.log("map", userSocketMap);
+      } else {
+        console.log("Recipient socket not found for user ID:", recipientId);
+      }
 
-  //     // Optionally, you can also emit the message to the sender
-  //     const senderSocketId = userSocketMap[senderId];
-  //     if (senderSocketId) {
-  //       io.to(senderSocketId).emit("recive", {
-  //         message: msg,
-  //         senderId,
-  //         recipientId,
-  //         name,
-  //         timestamp: Date.now()
-  //       });
-  //       console.log("map", userSocketMap);
-  //     } else {
-  //       console.log("Sender socket not found for user ID:", senderId);
-  //     }
+      // Optionally, you can also emit the message to the sender
+      const senderSocketId = userSocketMap[senderId];
+      if (senderSocketId) {
+        io.to(senderSocketId).emit("recive", {
+          message: msg,
+          senderId,
+          recipientId,
+          name,
+          timestamp: Date.now()
+        });
+        console.log("map", userSocketMap);
+      } else {
+        console.log("Sender socket not found for user ID:", senderId);
+      }
 
-  //     // Save the message to the database or perform other actions as needed
-  //     if (!senderId) {
-  //       throw new Error("Sender ID is missing or invalid");
-  //     }
+      // Save the message to the database or perform other actions as needed
+      if (!senderId) {
+        throw new Error("Sender ID is missing or invalid");
+      }
 
-  //     await saveMessageToDatabase(senderId, recipientId, msg);
-  //   } catch (error) {
-  //     console.error("Error handling message:", error);
-  //   }
-  // });
+      await saveMessageToDatabase(senderId, recipientId, msg);
+    } catch (error) {
+      console.error("Error handling message:", error);
+    }
+  });
 
-  // // Add the socket ID to the userSocketMap when a user connects
-  // socket.on("userId", userId => {
-  //   userSocketMap[userId] = socket.id;
-  // });
+  // Add the socket ID to the userSocketMap when a user connects
+  socket.on("userId", userId => {
+    userSocketMap[userId] = socket.id;
+  });
 
-  // // Remove the socket ID from the userSocketMap when a user disconnects
-  // socket.on("disconnect", () => {
-  //   for (const userId in userSocketMap) {
-  //     if (userSocketMap[userId] === socket.id) {
-  //       delete userSocketMap[userId];
-  //       break;
-  //     }
-  //   }
-  //   console.log("Socket disconnected:", socket.id);
-  // });
+  console.log("socket me", socket.id);
+  socket.emit("me", socket.id);
+
+  socket.on("disconnected", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", data => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name
+    });
+  });
+
+  socket.on("answerCall", data => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+
+  // Remove the socket ID from the userSocketMap when a user disconnects
+  socket.on("disconnect", () => {
+    for (const userId in userSocketMap) {
+      if (userSocketMap[userId] === socket.id) {
+        delete userSocketMap[userId];
+        break;
+      }
+    }
+    console.log("Socket disconnected:", socket.id);
+  });
 });
 
 // Define the schema for messages
